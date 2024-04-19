27ca85bfa7c13c8e93cc1be6194d7ad1ee278e11
Heerak J <heerakj@Heeraks-MacBook-Air.local>
2024-04-19 13:05:34 +0530

    added third phase

--- App.js
+++ App.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
 
-const heading = React.createElement("h1",{id:'heading'},"Hey Heerak. Here is React")
-const roots = ReactDOM.createRoot(document.getElementById('headerroot'))
-roots.render(heading)
+// JSX is not HTML inside javascript but you can call it as HTML like syntax.
+// Jsx is transpiled before it reaches the JS engine as JSX is not understandable by JS engine.
+// This transpilation is done by PARCEL here that contains BABEL that does the work.
+const Title = () => (
+    <h1 className='heerak'>
+        Hi Heerak
+    </h1>
+)
+
+const HeadingComponent = ()=>(
+    <div id='container'>
+        <Title></Title>
+        <Title/>
+        {Title()}
+        <h1 className='heading'>
+            Namaste Heerak, Thanks for practicing react.
+        </h1>
+    </div>
+)
+const root = ReactDOM.createRoot(document.getElementById('headerRoot'))
+root.render(<HeadingComponent />)
